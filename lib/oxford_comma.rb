14860c6df0ecf8b8afe6dc302d66b6b652775719
Brittany Hartmire <bkhartmire@gmail.com>
2017-11-18 14:17:56 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-  array.join(", ")
+  if array.size == 1
+    return array[0]
+  elsif array.size == 2
+    return "#{array[0]} and #{array[1]}"
+  else 
+    new_string = ""
+    counter = 0
+    while counter < array.size
+      string = array.join(", ")
+      counter += 1
+    end
+    new_string.push(string)
+    new_string.push(" and #{array.last}")
+  end
 end
